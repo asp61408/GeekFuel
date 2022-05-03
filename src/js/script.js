73e3bff06e8a631708e5d7e0481f779b65b61f8d
Alex <aspushkin2015@gmail.com>
2022-05-03 12:32:30 +0300

    +fandoms,footer sections,+slider compliment

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// slider from skick
+$(document).ready(function () {
+    $(".compliment__wrapper").slick({
+        speed: 1200,
+        // controls: true,
+        // dots: true,
+        arrows: false,
+        pauseOnHover: true,
+        autoplay: true
+    });
+});
